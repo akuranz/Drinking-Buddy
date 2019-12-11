dab85da2864e096052ad47ae66d0cef18b01ff49
Inbary <55555231+Inbary-s@users.noreply.github.com>
2019-12-10 17:42:04 -0700

    fixed cta buttons style and open in new window, cleaned unused code

--- javascript/JQueryForReturnData.js
+++ javascript/JQueryForReturnData.js
@@ -8,7 +8,6 @@ $(".out").on("click", function() {
 
 $("#search-brewery").on("click", function(e) {
   e.preventDefault();
-
   var APIKey = "10418444aeb3ca5b2578412ce0662909";
   var city = $("#find-brewery")
     .val()
@@ -32,7 +31,7 @@ $("#search-brewery").on("click", function(e) {
     console.log(response);
     $(".carousel").empty();
     for (var i = 0; i < response.length; i++) {
-      var breweryCard = $("<div>").attr("class", "item-1 card");
+      var breweryCard = $("<div>").attr("class", "card");
       var breweryInfo = [
         $("<img>").attr("src", "assets/images/brewery.png"),
         $("<h4>").text(response[i].name),
@@ -48,10 +47,9 @@ $("#search-brewery").on("click", function(e) {
         "cta row justify-content-center"
       );
       var contactInfo = [
-        $("<i>")
-          .addClass("fas fa-globe-americas url")
+        $("<a>")
           .attr("href", "http://www." + response[i].url)
-          .attr("target", "_blank"), //how do we open in a new window?
+          .attr("target", "_blank"), 
         $("<i>")
           .addClass("fas fa-phone phone")
           .attr("href", "tel:+01" + response[i].phone),
@@ -62,27 +60,19 @@ $("#search-brewery").on("click", function(e) {
           .attr("href", "directions.html")
       ];
 
+      contactInfo[0].append(`<i class='fas fa-globe-americas url'>`)
       $(breweryContact).append(contactInfo);
       $(breweryCard).append(breweryContact);
       $(".carousel").append(breweryCard);
     }
-
     console.log($("#directions").attr("data-id"));
   });
 });
-
-$(document).on("click", ".url", function(e) {
-  e.preventDefault();
-  window.location.href = $(this).attr("href");
-  console.log($(this));
-});
-
 $(document).on("click", ".phone", function(e) {
   e.preventDefault();
   window.location.href = $(this).attr("href");
   console.log($(this).attr("href"));
 });
-
 $(document).on("click", ".directions", function(e) {
   e.preventDefault();
   var APIKey = "10418444aeb3ca5b2578412ce0662909";
@@ -94,16 +84,13 @@ $(document).on("click", ".directions", function(e) {
     "/" +
     ID +
     "&s=json";
-
   $.ajax({
     url: queryURLbeerMappingLocInfo,
     method: "GET"
   }).then(function(response) {
     console.log(response);
-
     var latitude = response[0].lat;
     var longitude = response[0].lng;
-
     breweryLocation = {
       lat: JSON.parse(latitude),
       lng: JSON.parse(longitude)
@@ -112,7 +99,6 @@ $(document).on("click", ".directions", function(e) {
     var stringify = JSON.stringify(breweryLocation);
     console.log(stringify);
     localStorage.setItem("Brewery Lat/Long", stringify);
-
     window.location.href = "directions.html";
     console.log($(this).attr("href"));
   });
